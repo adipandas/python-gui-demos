cf6bdcd1545bb0ea9779f8675d316a01b745820c
Aditya <adityadeshpande2010@gmail.com>
2017-08-12 18:40:21 -0400

    Add main program to import all demo programs and run those.

---
+++ src/main.py
@@ -0,0 +1,16 @@
+'''
+Created on Aug 12, 2017
+
+@author: Aditya
+
+This is the main file and will import other modules/codes written for python
+'''
+
+import program1 as p1
+import program2 as p2
+
+def main():
+    p1.sayhello()
+    p2.HelloAppLaunch()
+
+if __name__ == '__main__':main()
