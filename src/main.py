20d5b44155b847c16254f7be57dad67c96aeb848
Aditya <adityadeshpande2010@gmail.com>
2017-08-14 13:00:07 -0400

    Import program3 in main

--- src/main.py
+++ src/main.py
@@ -8,9 +8,11 @@ This is the main file and will import other modules/codes written for python
 
 import program1 as p1
 import program2 as p2
+import program3 as p3
 
 def main():
     p1.sayhello()
     p2.HelloAppLaunch()
+    p3.GreetingAppLaunch()
 
 if __name__ == '__main__':main()
