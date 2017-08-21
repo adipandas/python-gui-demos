721dcfe1c99bdf200eff84c32adee4e407a7da87
Aditya <adityadeshpande2010@gmail.com>
2017-08-21 01:28:04 -0400

    Modify simple_gif image, add demo for button widget

--- src/main.py
+++ src/main.py
@@ -9,10 +9,12 @@ This is the main file and will import other modules/codes written for python
 import program1 as p1
 import program2 as p2
 import program3 as p3
+import program4 as p4
 
 def main():
     p1.sayhello()
     p2.HelloAppLaunch()
     p3.GreetingAppLaunch()
+    p4.launchButtonApp()
 
 if __name__ == '__main__':main()
---
+++ src/program4.py
@@ -0,0 +1,75 @@
+'''
+Created on Aug 20, 2017
+
+@author: Aditya
+
+This program demonstrates the use of button widget in tkinter
+'''
+import tkinter as tk
+from tkinter import ttk
+
+class ButtonApp:
+    def __init__(self, master):
+        self.button = ttk.Button(master, text = 'Click me')
+        self.button.pack()
+        self.button.config(command = self.buttonfunc)          # configure a command for button click
+        
+        self.btn1 = ttk.Button(master, text = 'Click on \'Click me\'', command = self.invokebutton)
+        self.btn1.pack()
+        
+        self.btn2 = ttk.Button(master, text = 'Disable \'Click me\'', command = self.disableButton)
+        self.btn2.pack()
+        
+        self.btn3 = ttk.Button(master, text = 'Enable \'Click me\'', command = self.enableButton)
+        self.btn3.pack()
+        
+        self.btn4 = ttk.Button(master, text = 'Query state of \'Click me\'', command = self.queryButtonState)
+        self.btn4.pack()
+        
+        self.button.img  = tk.PhotoImage(file = 'simple_gif.gif')
+        self.button.img = self.button.img.subsample(10, 10) # take every 5th pixel in x and y direction of image
+        
+        self.btn5 = ttk.Button(master, text = 'Add image to \'Click me\'', command = self.addImage)
+        self.btn5.pack()
+        
+        self.label = ttk.Label(master, text = 'No button pressed yet.')
+        self.label.pack()
+        
+    def buttonfunc(self):
+        self.label.config(text = 'Clicked!!')
+    
+    def invokebutton(self):
+        self.button.invoke()    # invoke the button
+        
+    def disableButton(self):
+        if self.button.instate(['disabled']):
+            self.label.config(text='\'Click me\' is already disabled.')
+        else:
+            self.button.state(['disabled'])      # returns the previous state of button and sets the new state
+            self.label.config(text = '\'Click me\' is disabled.')
+        
+    def queryButtonState(self):
+        if self.button.instate(['disabled']):
+            self.label.config(text = '\'Click me\' is in diabled state.')
+        else:
+            self.label.config(text = '\'Click me\' is in enabled state.')
+    
+    def enableButton(self):
+        if self.button.instate(['!disabled']):
+            self.label.config(text = '\'Click me\' is already enabled')
+        else :
+            self.button.state(['!disabled'])
+            self.label.config(text = '\'Click me\' is enabled')
+    
+    def addImage(self):
+        self.button.config(image = self.button.img, compound = tk.LEFT)
+            
+def launchButtonApp():
+    root = tk.Tk()  # instantiate the main window class by creating its object
+    ButtonApp(root)
+    tk.mainloop()
+
+def test():
+    launchButtonApp()
+
+if __name__ == '__main__': test()
