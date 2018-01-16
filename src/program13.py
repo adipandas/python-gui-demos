dc4ac8aea24e9587d001c2ea0b15f010cd8da86c
Aditya <adityadeshpande2010@gmail.com>
2018-01-15 20:49:20 -0500

    demo of tags in text widget

--- src/program13.py
+++ src/program13.py
@@ -17,6 +17,12 @@ Indexing in text widget:
 - 'end' refers to the position after the last character of a line
 - Please refer the indices section of tkinter documentation of Text Widget for more details
 
+Tags can be created in text widget to modify words/text at various locations with some logic.
+Tags are mapped to the specified locations/indices of text in Text widget.
+So it is possible to use these tags for text modification instead of text indices. 
+Therefore, making it convenient for programming.
+
+
 '''
 
 import tkinter as tk
@@ -58,10 +64,49 @@ class App(object):
         self.btn8 = ttk.Button(self.master, text = 'Disable Text Field', command = self.disableEnable)
         self.btn8.pack()
         
-        
         self.lbl1 = ttk.Label(self.master, text = 'Waiting to display TEXT....')
         self.lbl1.pack()
-    
+        
+        
+        self.btn9 = ttk.Button(self.master, text = 'Create tag named \'myTag\' at line 2', command = self.tagDemo)
+        self.btn9.pack()
+        
+    def tagDemo(self):
+        if self.btn9['text']=='Create tag named \'myTag\' at line 2':
+            self.btn9.config(text = 'Remove Tag')
+            self.text.tag_add('myTag', '2.0', '2.0 lineend')
+            
+            self.btn10 = ttk.Button(self.master, text = 'Change myTag background to yellow', command = self.tagbgyellow)
+            self.btn10.pack()
+            
+            self.btn11 = ttk.Button(self.master, text = 'Remove tag from 1st word of line 2', command = self.tagrm21word)
+            self.btn11.pack()
+            
+            self.btn12 = ttk.Button(self.master, text = 'myTag Span', command = self.getTagSpan)
+            self.btn12.pack()
+            
+            self.btn13 = ttk.Button(self.master, text = 'Show all Tags in Text widget', command = self.displayAllTags)
+            self.btn13.pack()
+            
+        else:
+            self.btn9.config(text = 'Create tag named \'myTag\' at line 2')
+            self.text.tag_delete('myTag')
+            self.btn10.destroy()
+            self.btn11.destroy()
+            self.btn12.destroy()
+            self.btn13.destroy()
+            
+    def getTagSpan(self):
+        self.lbl1.config(text = self.text.tag_ranges('myTag'))
+        
+    def displayAllTags(self):
+        self.lbl1.config(text = self.text.tag_names())
+        
+    def tagrm21word(self):
+        self.text.tag_remove('myTag', '2.0', '2.0 wordend')
+    def tagbgyellow(self):
+        self.text.tag_configure('myTag', background = 'yellow')
+        
     def randomTextEntry(self):
         text = '''Random Text
 It real sent your at. 
